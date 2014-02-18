e146e1852f09ef05bc6e898dd448bf283386a8cd
phmulin <philipp.gutheim@gmail.com>
2014-02-17 20:56:53 -0800

    UI Change of Email lists
    Changed how emails are displayed in the inbox. Removed all icons and
    buttons and expanded sender, subject, content fields to fit full area.

--- js/updateUI.js
+++ js/updateUI.js
@@ -17,6 +17,23 @@ function cleanupGmailUI() {
   //alter left nativation bar, remove "move" (.n6) and labels (.zw)
   $("div.n6").css("display","none");
   $("div.zw").css("display","none");
+
+  //Change how email list is displayed. Remove bottons and icons on the left and space out content
+  //add id to email list
+  $("table.F.cf.zt").attr('id','pmail-inbox-list');
+  //Select email list table and set all icons to hide.
+  $("#pmail-inbox-list").find("td.PF.xY, td.oZ-x3.xY.aid, td.apU.xY, td.WA.xY").hide();
+  //remove the colgroup in table
+  $("#pmail-inbox-list").find("colgroup").remove();
+  //Set new email list layout
+  var emaillist = $("#pmail-inbox-list").children().children(); 
+  $.each( emaillist, function(index, emailrow) {
+    $(emailrow).find("td:eq(4)").width(150);
+    $(emailrow).find("td:eq(5)").width("auto");
+    $(emailrow).find("td:eq(6)").width(24);
+    $(emailrow).find("td:eq(7)").width(50);
+  });
+
   //do initial removal of navigation and ad above emails
   cleanupGmailUIOnEvent();
   //initialize loadPamilUI() via init()
@@ -25,16 +42,14 @@ function cleanupGmailUI() {
 
 function cleanupGmailUIOnEvent(){
   //remove bottons, navs, and ads above emails
-  $("div.D.E.G-atb.bP").parent().css("display","none");
-  $("div.D.E.G-atb.bP").parent().next().css("display","none");
+  $("div.D.E.G-atb.bP").parent().hide();
+  $("div.D.E.G-atb.bP").parent().next().hide();
+  //$("div.nH.aqK").hide();//navbar on non-inbox sections
 }
 
 
 //loads the initial PmailUI
 function loadPmailUI() {
-  
-  // need to load the right hand side AI Dashboard
-
   // add id and spacing
   $("div.nH.oy8Mbf.nn.aeN").next().find("div.nH.nn").attr('id', 'pmail-inbox');
   $("div.nH.oy8Mbf.nn.aeN").next().find("div.nH.nn").after('<div class="nH nn" id="pmail-inbox-spacing" style="width: 4px; height: 4px;"></div>');
@@ -44,7 +59,7 @@ function loadPmailUI() {
   <div class='nH nn' id='pmail-aidashabord' style='width: 532.8px;'>\
     <div class='nH'>\
       <div class='BltHke nK' role='main'>\
-        <div class='nH Cq'><div class='nJ A2'>Awaiting Response</div></div>\
+        <div class='nH Cq'><div class='nJ A2'>Current Action Items</div></div>\
         <div class='awe4 UI UJ' gh='tl'>\
           <div class='Cp'>\
             <div>\
